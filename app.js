e029671378f0171fba0b3fdcf11b3241065918cd
Kiran <akiran547@gmail.com>
2017-05-04 22:38:25 +0530

    echo in graphql

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 import express from 'express';
-console.log(express);
+import graphqlHTTP from 'express-graphql';
+import {
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLObjectType
+} from 'graphql';
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQuery',
+        fields: {
+            echo: {
+                type: GraphQLString,
+                resolve() {
+                    return 'Echo.. ' + new Date()
+                }
+            }
+        }
+    })
+});
+
+const app = express();
+app.use('/graphql', graphqlHTTP(function(){
+    return {
+        schema,
+        graphiql: true,
+        pretty: true
+    }
+}));
+
+var server = app.listen(3000, function(){
+    console.log('app listening on port', server.address().port);
+});
+
+
